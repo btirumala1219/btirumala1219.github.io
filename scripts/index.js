3534cd8cd28f3ef88922e58c8ca5d7e8d447247d
btirumala <barath.tirumala@gmail.com>
2018-12-23 00:16:27 -0500

    Version 2 live

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-function move(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-
-  var dom = document.getElementById("overlaybody");
-  dom.style.background= "radial-gradient(transparent, red);"
-  console.log(dom.style.background);
-  //background:radial-gradient(transparent, #f9f8fa);
-}
